aad6cffa575ba7393ab14b09b5c859c5fc9cdd1a
lokendra1398 <73362947+lokendra1398@users.noreply.github.com>
2021-07-08 00:38:27 +0530

    Delete RFQ directory

--- RFQ/rfq/src/main/java/com/cts/repository/RFQRepo.java
+++
@@ -1,9 +0,0 @@
-package com.cts.repository;
-
-import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-@Repository
-public interface RFQRepo extends JpaRepository<RFQservice, Integer> {
-
-}
