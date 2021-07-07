980501b67b13ec9eb6356e548d3d6496c9599e18
lokendra1398 <73362947+lokendra1398@users.noreply.github.com>
2021-07-08 00:39:11 +0530

    Add files via upload

---
+++ RFQModule/src/main/java/com/cts/controller/RFqController.java
@@ -0,0 +1,42 @@
+package com.cts.controller;
+import java.util.List;
+import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.cts.model.RFQ;
+import com.cts.RFQservice.RFQService;
+
+
+@RestController
+@RequestMapping("/vendors")
+public class RFqController 
+{
+	private static final Logger log = LoggerFactory.getLogger(RFqController.class);
+	@Autowired
+    private RFQService  rfqService;
+    @GetMapping("/getRFQOfPlant/{Plantid}")
+    public List<RFQ> getRFQOfPlant(@PathVariable Integer Plantid){
+    	log.info("Inside.getRFQOfPlant of RFqController");
+    	return rfqService.findAll();
+    }
+    
+//   //@GetMapping("/getPotentialVendorsOfRFQ/{RFQId}")
+//   public ResponseEntity<Integer> getPotentialVendorsOfRFQ(@PathVariable Integer RFQId)  {
+//	   log.info("Inside getPotentialVendorsOFRFQ of RFQController");
+//	   Optional<RFQ> rfq = rfqService.findbyRFQId(RFQId);
+//	   if ( rfq !=null)
+//	   {
+//		  // Integer partId = rfq.get().getPartId();
+//		  // return new ResponseEntity<Integer>(partId, HttpStatus.OK);
+//	   }else
+//		    return new ResponseEntity<Integer>(HttpStatus.NOT_FOUND);
+//   }
+		   
+}
---
+++ RFQModule/src/main/java/com/cts/model/RFQ.java
@@ -0,0 +1,66 @@
+package com.cts.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class RFQ {
+    @Id
+    @Column(name = "RFQID")
+	private int rfqId;
+    @Column(name ="PARTID")
+	private int partId;
+    @Column(name ="QUANTITY")
+	private int quantity;
+    @Column(name ="PartName")
+	private int partName;
+    @Column(name ="SPECIFICATION")
+	private String Specification;
+    @Column(name ="TIMETOSUPPLY")
+	private int timetoSupply;
+    
+    public RFQ() {
+    	super();
+    }
+	public int getRfqId() {
+		return rfqId;
+	}
+	public void setRfqId(int rfqId) {
+		this.rfqId = rfqId;
+	}
+	public int getPartId() {
+		return partId;
+	}
+	public void setPartId(int partId) {
+		this.partId = partId;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public int getPartName() {
+		return partName;
+	}
+	public void setPartName(int partName) {
+		this.partName = partName;
+	}
+	public String getSpecification() {
+		return Specification;
+	}
+	public void setSpecification(String specification) {
+		Specification = specification;
+	}
+	public int getTimetoSupply() {
+		return timetoSupply;
+	}
+	public void setTimetoSupply(int timetoSupply) {
+		this.timetoSupply = timetoSupply;
+	}
+	
+	
+
+
+}
