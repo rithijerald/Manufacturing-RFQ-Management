d28bb29e03fb0818fd54bdc698835229dd6a7f9d
jahanvi03 <64686390+jahanvi03@users.noreply.github.com>
2021-07-08 09:48:41 +0530

    Update PartRepository.java

--- PlantModule/src/main/java/com/repository/PartRepository.java
+++ PlantModule/src/main/java/com/repository/PartRepository.java
@@ -2,7 +2,7 @@ package com.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import org.springframework.data,repository.Param;
+import org.springframework.data.repository.Param;
 import com.model.Part;
 
 @Repository
